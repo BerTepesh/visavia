edc8ab03596f751e3976be878da289f8ee2122a3
BerTepesh <Ber.Tepesh@gmail.com>
2020-01-24 14:31:46 +0300

    Калькулятор

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 let setProgress = function() {
-  $(".section").each(function(){
+  $(".anim-section").each(function(){
     let section = $(this).offset().top;
     let viewSpot = $(window).scrollTop() + $(window).height()/1.3;
     if(viewSpot > section) {
@@ -25,5 +25,11 @@ $(".ham-menu-trigger").click(() => {
   $(".ham-menu").toggleClass("active");
   $(".ham-menu-trigger").toggleClass("ham-menu-trigger_close")
 });
-
-
+$(".amount__btn").click(function(){
+  let num = $(this).closest(".amount").find(".amount__number");
+  if($(this).hasClass("amount__btn_minus") && num.val() > 0) {
+    num.val(num.val() - 1);
+  } else if($(this).hasClass("amount__btn_plus")) {
+    num.val(parseInt(num.val()) + 1);
+  }
+});
